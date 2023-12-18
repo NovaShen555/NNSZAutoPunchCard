aa3e70840544423d1b8d914e64f13bac998f6941
NovaShen555 <admin@novashen.top>
2023-12-18 19:20:47 +0800

    update punch time

--- autoPunch.py
+++ autoPunch.py
@@ -17,15 +17,21 @@ def your_task():
         for row in reader:
             # 尝试获取cardId是否合法
             time.sleep(2)
-            ck = punchCard(row[0], row[1])
-            print(row[0], row[1])
-            # ck = {'code': 200}
-            print(getTime()+str(ck))
-            if ck['code'] == 200:
-                # 将数据写入CSV文件
-                with open('output.csv', 'a', newline='') as file:
-                    writer = csv.writer(file)
-                    writer.writerow([row[0], getTime()])
+            try:
+                ck = punchCard(row[0], row[1])
+                time.sleep(2)
+                print(row[0], row[1])
+                # ck = {'code': 200}
+                print(getTime()+str(ck))
+                if ck['code'] == 200:
+                    # 将数据写入CSV文件
+                    with open('output.csv', 'a', newline='') as file:
+                        writer = csv.writer(file)
+                        writer.writerow([row[0], getTime()])
+            except:
+                print(getTime()+'error')
+                time.sleep(2)
+                continue
 
 # 创建一个调度器对象
 scheduler = schedule.Scheduler()
